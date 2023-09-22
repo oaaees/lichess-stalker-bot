ed29cec2d26056e8b34088101babb51207546c9f
Johan Paredes <jmpareds@gmail.com>
2023-09-21 21:47:35 -0400

    added support for user input

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 import urllib.request
 import json
 
-contents = urllib.request.urlopen("http://lichess.org/api/users/status?ids=capibarazul").read()
+user = input("Enter lichess username to track: ")
+
+contents = urllib.request.urlopen("http://lichess.org/api/users/status?ids=" + user).read()
 contents = contents.decode('utf8').replace("'", '"')
 data = json.loads(contents)
 
 if "online" in data[0].keys() and data[0]["online"] == True:
-    print("oaaees is online!")
+    print(user + " is online!")
 else : 
     print("not online")
 
